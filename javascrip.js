256623db9702b4fdc7669bb474fc01b253d12211
futakih <fikkun18@gmail.com>
2021-01-15 19:58:44 +0700

    directory js

--- javascrip.js
+++ javascrip.js
@@ -1,29 +1,5 @@
 let menuToggle=document.querySelector('.menu-toggle');
 let menu=document.querySelector('.menubar');
-let regbtn=document.querySelector('.regBtn');
-let kotakreg=document.querySelector('.kotak_Registrasi');
-let kotaksig=document.querySelector('.kotak_masuk');
-let btnlog=document.getElementById('btnlogin');
-
-function validasi(){
-    var username = document.forms["iniform"]["username"].value;
-    var password = document.forms["iniform"]["password"].value;
-    if( username == "Taufik" && password == "123123"){
-        modalsukses.classList.add("normal");
-    }
-    else{
-        modalgagal.classList.add("normal");
-    
-    }
-}
-btnlog.addEventListener('click',function(){
-    validasi()
-})
-
-regbtn.addEventListener('click',function(){
-    kotakreg.classList.add('normal');
-    kotaksig.classList.add('kotak_Registrasi')
-});
 
 menuToggle.addEventListener('click', function(){
     menu.classList.toggle('normal')
